e206a9544c22c57ff0d955fbd31b540c7d4488a6
sania089 <sania.sarin@somaiya.edu>
2024-04-14 13:58:18 +0530

    final

---
+++ Job-Microservices-main/jobms/jobms/src/main/java/com/embarkx/jobms/job/JobService.java
@@ -0,0 +1,16 @@
+package com.embarkx.jobms.job;
+
+import com.embarkx.jobms.job.dto.JobWithCompanyDTO;
+
+import java.util.List;
+
+public interface JobService {
+    List<JobWithCompanyDTO> findAll();
+    void createJob(Job job);
+
+    JobWithCompanyDTO getJobById(Long id);
+
+    boolean deleteJobById(Long id);
+
+    boolean updateJob(Long id, Job updatedJob);
+}
